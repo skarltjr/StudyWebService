90b76eb00083d5ec6b0190f599fbac2cd22fd195
skarltjr <62214428+skarltjr@users.noreply.github.com>
2020-11-13 17:09:08 +0900

    알림을 Interceptor로 풀어나가기

--- src/main/java/com/studyolle/modules/notification/NotificationInterceptor.java
+++ src/main/java/com/studyolle/modules/notification/NotificationInterceptor.java
@@ -13,20 +13,28 @@ import org.springframework.web.servlet.view.RedirectView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/** 모든 요청(홈으로 가거나 다른 스터디로 가거나 했을 때를 포함해서)에서 알림이 오면 알림아이콘 색이 변하도록 설정하고싶다
+ * 그럼 모든 요청에 대해 이벤트는 처리하고 ★뷰를 렌더링 하기 전에 postHandle
+ * 알림이 생성되었다는 것 자체가 예를들어 내가 관심있는 지역에 대한 스터디가 만들어지고 studyCreatedEvent가 핸들러로
+ * 처리가 되었다는 뜻 그러니 post
+ * pre 는 핸들러 실행 전 post핸들러 실행 후 뷰 렌더링 전 after는 뷰 렌더링 이후*/
+
 @Component
 @RequiredArgsConstructor
 public class NotificationInterceptor implements HandlerInterceptor {
     private final NotificationRepository notificationRepository;
 
-    //pre는 핸들러 처리전 post는 뷰 렌더링 전 after는 뷰 렌더링 후. 지금은 매 요청 뷰전에 알림을 미리 설정해놓고 뷰를 보여주기위해
      @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         //일단 인증된 계정에서만 작동해야하니까
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+
+         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (modelAndView != null && !isRedirectView(modelAndView) && authentication != null &&
                 authentication.getPrincipal() instanceof UserAccount) {
     /** model을 안쓰는 경우나 리다이렉트하는 경우나 인증이 안된경우나 .인증이 된 객체가 UserAccount타입이 아닌 경우에는 해당 x*/
 
+            // 중요한 관점 . 모든 계정에 대해서가아니라 현재 사용자 계정에 대해
             Account account = ((UserAccount) authentication.getPrincipal()).getAccount();
             long count = notificationRepository.countByAccountAndChecked(account, false);
             modelAndView.addObject("hasNotification", count > 0);
