1e5438663fee8a944ec8b6fa427166d595a40ef4
kiseok Nam <62214428+skarltjr@users.noreply.github.com>
2020-11-29 03:32:56 +0900

    Update AppProperties.java

--- src/main/java/com/studyolle/infra/config/AppProperties.java
+++ src/main/java/com/studyolle/infra/config/AppProperties.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@ConfigurationProperties("app")
+@ConfigurationProperties("app")   //spring_boot 에 정리해둔것과 마찬가지로 component로 올린 이 빈을 재정의해서 사용하기보단 빈으로 등록하고 application.properties에서 값만 수정하여 쓰도록
 public class AppProperties {
 
     private String host;
